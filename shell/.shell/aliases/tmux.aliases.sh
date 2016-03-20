2781a9ef7a29a257b21334b61df9d31fbfce407a
sejgit <sejgit@github.com>
2016-03-20 19:28:44 -0400

    add dependancies.org

--- shell/.shell/aliases/tmux.aliases.sh
+++ shell/.shell/aliases/tmux.aliases.sh
@@ -2,7 +2,8 @@
 # tmux.aliases.sh
 # sej 2016 03 15
 
-alias tmux="TERM=xterm-256color tmux -f ~/.config/.tmux.conf "
+# alias tmux="TERM=xterm-256color tmux -f ~/.config/.tmux.conf "
+alias tmux="tmux -f ~/.config/.tmux.conf "
 alias txl='tmux ls'
 alias txn='tmux new -s'
 alias txa='tmux a -t'

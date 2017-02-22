dcde625993a38076769add1e7bcf207dfb3089c4
sej <sejgit@users.noreply.github.com>
2017-02-22 12:43:32 -0500

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # dependancies.sh
 # 2016 03 23
+# 2017 02 22 update
 
 
-sudo apt-get install git emacs24 stow tmux
-
-
-
-#ln -s ~/dotfiles/todo.txt-cli/todo.sh ~/dotfiles/shell/bin/todo.sh
-#ln -s ~/dotfiles/todo.txt-cli/todo_completion ~/dotfiles/shell/completions/todo_completion.sh
-
+sudo apt-get install git emacs24 stow
+git submodule foreach init
+git submodule foreach sync
+git submodule foreach update

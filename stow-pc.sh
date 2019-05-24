fb4941f8b8dbf0535507c54a9d2712aa5931d89e
sejgit <sejgit@users.noreply.github.com>
2019-05-24 10:27:05 -0400

    stow-pc.sh update

--- stow-pc.sh
+++ stow-pc.sh
@@ -4,10 +4,10 @@
 # sej 2019 05 24 init
 
 # to get stow on mingw64 system
-# pacman -S mingw64/mingw-w64-x86_64-nodejs
+# pacman -S mingw64/mingw-w64-x86_64-perl
+# pacman -S mingw64/mingw-w64-x86_64-perl-docs
 # cpan App::cpanminus
-# cpanm Test::More Test::Output Log::Log4perl
-# cpanm YAML
+# cpanm Test::More Test::Output Log::Log4perl YAML
 # cpan <enter>
 # o conf yaml_module YAML <enter>
 # o conf commit <enter>

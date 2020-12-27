a98c550ad269144d33319069e126c14067de27ab
sejgit <sejgit@github.com>
2020-12-27 18:02:54 -0500

    update stow-install script for instructions

--- stow-pc.sh
+++ stow-pc.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# stow-pc.sh
+# stow-install.sh
 
 # sej 2019 05 24 init
+# update 2020 12 27 update for linux
 
 # to get stow on mingw64 system
 # pacman -S mingw64/mingw-w64-x86_64-perl
@@ -14,6 +15,12 @@
 # cpanm Stow --force  # it fails test on mingw
 
 
-stow -t ~/ --ignore=.bash_path -v shell
+# install stow from apt or brew
+stow -t ~/ -v shell
 
-# end of copybasics.sh
+# install screenfetch from apt or brew
+# install antibody from brew or
+# sudo curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+
+
+# end of stow-install.sh

84c067072d83da7bf9268b52fd0d9856e8c68373
sejgit <sejgit@github.com>
2021-02-27 17:07:24 -0500

    update

--- mini_emacs/raspbian.sh
+++ mini_emacs/raspbian.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt upgrade
 sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-keys A04A6C4681484CF1
 sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-keys 648ACFD622F3D138
-sudo echo -e "deb http://deb.debian.org/debian buster-backports main\n" >> /etc/apt/sources.list
+echo -e "deb http://deb.debian.org/debian buster-backports main\n" >> sudo tee /etc/apt/sources.list
 sudo apt update
 sudo apt install -t buster-backports emacs-nox
 cp mini_emacs ~/.emacs.d
